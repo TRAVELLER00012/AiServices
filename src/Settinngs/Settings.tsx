75117f5acc5c1ed49dd71d6ea62a49223cab4ff5
Daksh Kumar <k.daksh29@gmail.com>
2024-05-05 17:35:25 +0530

    create main page services

---
+++ src/Settinngs/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainPageNavBar from '../components/MainPageNavBar'
+import ParticleBackground from '../components/ParticleBackground'
+
+const Settings = () => {
+  return (
+    <>
+      <MainPageNavBar />
+      <ParticleBackground />
+    </>
+  )
+}
+
+export default Settings
